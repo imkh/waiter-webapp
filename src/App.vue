c55a9941f9c834baa0a4dd6814887532863426a1
SHagege <solalhagege@gmail.com>
2017-02-07 15:47:31 -0800

    Base project with VueJS, material design elements for landing page

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <md-card>
+      <md-card-header>
+        <div class="md-title">
+          Welcome to Waiter
+        </div>
+        <div class="md-suphead">
+          Skip the line
+        </div>
+      </md-card-header>
+      <md-card-content>
+        <md-input-container>
+          <md-input placeholder="Username"></md-input>
+        </md-input-container>
+        <md-input-container>
+          <md-input placeholder="Password"></md-input>
+        </md-input-container>
+        <md-button class="md-raised md-primary" v-on:click="Login()">Login</md-button>
+        <md-button class="md-raised md-primary" v-on:click="SignIn()">Sign In</md-button>
+      </md-card-content>
+    </md-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Roboto', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 30px;
+  margin-left: 160px;
+  margin-right: 160px;
+}
+</style>
